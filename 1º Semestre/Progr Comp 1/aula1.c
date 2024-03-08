14d7a211f36f686ddad0b4b5115c380da60a8d6b
Miguel Valentini <111707132+Migvalentini@users.noreply.github.com>
2024-03-07 21:28:01 -0300

    Aula 1

---
+++ 1º Semestre/Progr Comp 1/aula1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>; 
+#include <math.h>;
+
+int main() {
+    printf("Hello World!\n");
+    if (2 <= 4)
+    {
+        printf("menor que ");
+    }
+
+    int a;
+    
+    for( a = 10; a < 20; a = a + 1 ){
+      printf("value of a: %d\n", a);
+    }
+
+}
