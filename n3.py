42b1c445bc4ba1967a8f3e70780027dbf7074535
Miguel Valentini <111707132+Migvalentini@users.noreply.github.com>
2024-06-11 20:34:34 -0300

    ok

--- n3.py
+++ n3.py
@@ -1,8 +1,6 @@
-n1 = int(input("Digite a nota 1: "))
-n2 = int(input("Digite a nota 2: "))
+n1 = float(input("Digite a nota 1: "))
+n2 = float(input("Digite a nota 2: "))
 
 n3 = 1/((1/2)-(1/n1)-(1/n2))
-
-print((1/2)-(1/n1)-(1/n2))
 
 print("N3: ", n3)
