db475f6611ea040fecfb0ba58f268b13eb77ecf6
Miguel Valentini <miguelvalentini2006@gmail.com>
2024-11-20 17:13:32 -0300

    Lista 11

---
+++ 2º Semestre/Programacao de Computadores 2/lista11/ex1.c
@@ -0,0 +1,14 @@
+// Considere um arquivo texto que contém os nomes e as notas dos alunos de uma disciplina. 
+// As informações de cada aluno ocupam duas linhas do arquivo: a primeira linha contém o nome do aluno (com eventuais espaços em branco) e a segunda as três notas obtidas. Conside
+// Fulano de Tal
+// 6.3 5.7 7.1
+// Sicrano Silva
+// 3.4 5.4 4.7
+// Beltrano Alves
+// 9.2 6.8 8.3
+// Escreva um programa que leia o arquivo “entrada.txt”, que contém as informações dos alunos no formato descrito acima, e imprima na tela o número de alunos aprovados e 
+// o número de alunos reprovados. Considere que um aluno é aprovado se obtiver média maior ou igual a cinco. 
+// Se não for possível abrir o arquivo, o programa deve ter como saída a mensagem “ERRO”. Para o exemplo ilustrado acima, a saída do programa seria:
+// Aprovados: 2
+// Reprovados: 1
+
