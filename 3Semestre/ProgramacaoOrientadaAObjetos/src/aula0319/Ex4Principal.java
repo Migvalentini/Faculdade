e8ab5737dc3eb0504b58fff06b3e8e25b6c8c498
mvalentini3 <mvalentini3@CX71LINF091667.ucs.local>
2025-03-26 19:40:57 -0300

    Ex4

---
+++ 3Semestre/ProgramacaoOrientadaAObjetos/src/aula0319/Ex4Principal.java
@@ -0,0 +1,20 @@
+package aula0319;
+
+import java.util.Scanner;
+
+public class Ex4Principal {
+
+	public static void main(String[] args) {
+		// Faça um programa em java que receba o valor do salário mínimo e o valor do salário de um funcionário, 
+		// calcule e mostre a quantidade de salários mínimos que ganha esse funcionário.
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o salário: ");
+		double salario = sc.nextDouble();
+		
+		System.out.println("O funcionario recebe " + salario / Ex4Salario.getValor() + " salários mínimos");
+		
+		sc.close();
+	}
+
+}
---
+++ 3Semestre/ProgramacaoOrientadaAObjetos/src/aula0319/Ex4Salario.java
@@ -0,0 +1,11 @@
+package aula0319;
+
+public class Ex4Salario {
+	private static final double VALOR = 1518;
+
+	public static double getValor() {
+		return VALOR;
+	}
+	
+	
+}
