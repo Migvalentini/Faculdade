439aab24cc5d88bfe721263c371d23f8e06ed779
Miguel Valentini <suportegautica@gmail.com>
2025-04-01 23:34:11 -0300

    Ex2 Biblioteca

---
+++ 3Semestre/ProgramacaoOrientadaAObjetos/src/aula0326/Ex2Biblioteca/Livro.java
@@ -0,0 +1,32 @@
+package aula0326.Ex2Biblioteca;
+
+import java.util.Date;
+
+public class Livro {
+	private String titulo;
+	private String autor;
+	private String isbn;
+	private Date datapublicacao;
+	private String categoria;
+	private StatusLivro status;
+	
+	public Livro(String titulo, String autor, String isbn, Date datapublicacao, String categoria, StatusLivro status) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.isbn = isbn;
+		this.datapublicacao = datapublicacao;
+		this.categoria = categoria;
+		this.status = (StatusLivro) status;
+	}
+
+	public String getTitulo() { return titulo; }
+	public String getAutor() { return autor; }
+	public String getIsbn() { return isbn; }
+	public Date getDatapublicacao() { return datapublicacao; }
+	public String getCategoria() { return categoria; }
+	public StatusLivro getStatusLivro() { return status; }
+}
+
+enum StatusLivro {
+    DISPONIVEL, EMPRESTADO, RESERVADO
+}
