aa5e4ff7594eee6383354293e47f486310f8c251
mvalentini3 <mvalentini3@CX71LINF222892.ucs.local>
2025-05-07 21:42:35 -0300

    aula 07-05

---
+++ 3Semestre/ProgramacaoOrientadaAObjetos/src/aula0507/Colaborador.java
@@ -0,0 +1,8 @@
+package aula0507;
+
+public interface Colaborador {
+	public static int valorHora = 20;
+	
+	//obtemSalario 
+	//cargaHoraria
+}
---
+++ 3Semestre/ProgramacaoOrientadaAObjetos/src/aula0507/Professor.java
@@ -0,0 +1,5 @@
+package aula0507;
+
+public interface Professor {
+	//obtemTitulacao
+}
