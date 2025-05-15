9ffd3a907eb263aa5069279c9a2d973f9d18dce2
mvalentini3 <mvalentini3@CX71LINF222925.ucs.local>
2025-05-14 22:00:48 -0300

    ok

---
+++ 3Semestre/ProgramacaoOrientadaAObjetos/src/aula0514/Ex1/Principal.java
@@ -0,0 +1,16 @@
+package aula0514.Ex1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// O código abaixo lança uma exceção (propositalmente) e interrompe sua
+		// execução. Utilizando o tratamento de exceções, corrija a classe com o objetivo
+		// de não parar sua execução.
+		// OBS:: A Exception lançada é ArrayIndexOutOfBoundsException
+		
+		System.out.println("Início do método");
+		
+	}
+
+	
+}
