8b96d10e74d30a58c0c20f4ca2b6f7c6d2541a07
Miguel Valentini <suportegautica@gmail.com>
2025-09-14 03:25:09 -0300

    ok

--- 3Semestre/AlgoritmosEstruturaDeDados/aula6/ex2.c
+++ 3Semestre/AlgoritmosEstruturaDeDados/aula6/ex2.c
@@ -1,4 +1,5 @@
 //Como você implementaria duas pilhas sobre o mesmo vetor? 
 //O vetor tem tamanho fixo (defina o tamanho), mas as pilhas não, o tamanho de cada uma pode variar tendo como restrição o tamanho do vetor 
 //e o espaço ocupado pela outra pilha. 
-//  Escreva o procedimento/método para empilhar e desempilhar, que possa ser utilizado para as duas pilhas.
+//Escreva o procedimento/método para empilhar e desempilhar, que possa ser utilizado para as duas pilhas.
+
