78fd5cd93ba16ae1991e105e591473c18dc978b3
Miguel Valentini <suportegautica@gmail.com>
2025-09-14 21:27:15 -0300

    ok

--- 3Semestre/AlgoritmosEstruturaDeDados/aula6/ex5.c
+++ 3Semestre/AlgoritmosEstruturaDeDados/aula6/ex5.c
@@ -1,3 +1,5 @@
 //Uma operadora possui 5 impressoras, cada impressora mantém uma fila de impressão.
 //Para enviar um trabalho para impressão, é preciso escolher a impressora através de seu identificador 
 //(pode ser sequencial, do 0 ao 4), utilizando a operação imprime( ).
+//Implemente o controle da operadora através da operação imprime(nroimpressora, trabalho). 
+//A operação imprime corresponde ao enfileira em uma impressora específica. Defina como seria o registro de cada impressão (nodo da lista).
--- 3Semestre/AlgoritmosEstruturaDeDados/aula6/ex6.c
+++ 3Semestre/AlgoritmosEstruturaDeDados/aula6/ex6.c
@@ -1,2 +1,6 @@
-//Implemente o controle da operadora através da operação imprime(nroimpressora, trabalho). 
-//A operação imprime corresponde ao enfileira em uma impressora específica. Defina como seria o registro de cada impressão (nodo da lista).
+//Uma operadora possui uma impressora, compartilhada por seus usuários. 
+//Essa impressora tem uma fila de impressão, para a qual os usuários enviam suas solicitações de impressão. 
+//Os usuários são classificados em 3 categorias: prioridade máxima (0), prioridade normal (1) e prioridade baixa (2). 
+//Para solicitar uma impressão, é utilizada a operação imprime(prioridade, trabalho), e a impressão ocorre através da operação impressao( ). 
+//A cada 2 impressões de prioridade máxima, a impressora imprime uma solicitação de prioridade normal. 
+//A cada 3 impressões de prioridade normal, a impressora imprime uma impressão de prioridade baixa. Implemente o controle da impressora.
